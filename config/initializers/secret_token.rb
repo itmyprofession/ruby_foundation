a85d5f8bc57988f7ab346d1ae68fe317ecdefad8
Santosh Moktan <itmyprofession@gmail.com>
2013-08-16 09:44:38 +0545

    Using Foundation with ROR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = '82329744c765bdc6c82e7c88d8eaa2def165457fb5af6c9d072a56d4c4fc2055b1dccc6c26fef7ee982036af7fbd7d504fe1f2879350664597725434b8aef4a4'
